34615f6852fb5c6dbf79792be587fd124e78ac55
Adam Shaw <ashaw1270@icloud.com>
2025-05-25 15:11:54 -0400

    Edit headers and intro

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -60,32 +60,20 @@ const Hero = () => {
   }, []);
 
   const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Brittany Chiang.</h2>;
-  const three = <h3 className="big-heading">I build things for the web.</h3>;
+  const two = <h2 className="big-heading">Adam Shaw.</h2>;
+  const three = <h3 className="big-heading">I make data talk.</h3>;
   const four = (
     <>
       <p>
-        I’m a software engineer specializing in building (and occasionally designing) exceptional
-        digital experiences. Currently, I’m focused on building accessible, human-centered products
-        at{' '}
-        <a href="https://upstatement.com/" target="_blank" rel="noreferrer">
-          Upstatement
-        </a>
-        .
+        I'm an aspiring machine learning engineer with interests in statistical learning theory and
+        the mathematical foundations that make modern ML work. I’m currently an Associate Software
+        Developer Intern at Google, where I’m working on integrating Gemini into Workspace products
+        (Docs, Sheets, etc.).
       </p>
     </>
   );
-  const five = (
-    <a
-      className="email-link"
-      href="https://www.newline.co/courses/build-a-spotify-connected-app"
-      target="_blank"
-      rel="noreferrer">
-      Check out my course!
-    </a>
-  );
 
-  const items = [one, two, three, four, five];
+  const items = [one, two, three, four];
 
   return (
     <StyledHeroSection>
