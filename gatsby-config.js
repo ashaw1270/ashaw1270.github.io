ad7a491de531e794c84cdbac5f8a050b85dd1f72
Adam Shaw <ashaw1270@icloud.com>
2025-07-05 16:42:51 -0500

    Change og

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,11 +3,7 @@ const config = require('./src/config');
 module.exports = {
   siteMetadata: {
     title: 'Adam Shaw',
-    description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
-    image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    siteUrl: 'https://ashaw1270.github.io', // No trailing slash allowed!
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
