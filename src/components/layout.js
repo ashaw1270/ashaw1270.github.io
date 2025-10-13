18adc51e3bfd4fc6170061ce8d29258c955862eb
Adam Shaw <ashaw1270@icloud.com>
2025-10-13 12:47:04 -0700

    Remove loading screen logo

--- src/components/layout.js
+++ src/components/layout.js
@@ -12,7 +12,7 @@ const StyledContent = styled.div`
 
 const Layout = ({ children, location }) => {
   const isHome = location.pathname === '/';
-  const [isLoading, setIsLoading] = useState(isHome);
+  const [isLoading, setIsLoading] = useState(false); // Disabled loader
 
   // Sets target="_blank" rel="noopener noreferrer" on external links
   const handleExternalLinks = () => {
