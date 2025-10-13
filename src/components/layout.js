aae6bb91d6778588a29a85c0abb37663694766ad
Adam Shaw <ashaw1270@icloud.com>
2025-10-13 12:51:01 -0700

    Revert "Remove loading screen logo"
    This reverts commit 18adc51e3bfd4fc6170061ce8d29258c955862eb.

--- src/components/layout.js
+++ src/components/layout.js
@@ -12,7 +12,7 @@ const StyledContent = styled.div`
 
 const Layout = ({ children, location }) => {
   const isHome = location.pathname === '/';
-  const [isLoading, setIsLoading] = useState(false); // Disabled loader
+  const [isLoading, setIsLoading] = useState(isHome);
 
   // Sets target="_blank" rel="noopener noreferrer" on external links
   const handleExternalLinks = () => {
