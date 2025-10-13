60a3bc8da4b90e8e2e0e18f2dca07df85da4b002
Adam Shaw <ashaw1270@icloud.com>
2025-10-13 12:51:59 -0700

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-e9d3a5409dd78a86b0bd.js"
+    "url": "webpack-runtime-d2744f5f507683dbdc3c.js"
   },
   {
     "url": "framework-5d6349cdf9c4e4fe213c.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "73d64b1ac4ee1d80ee2192ad238886e6"
+    "revision": "146874f505d7bf9df53d833685dc7877"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-6f061b09cc63f0400406.js"
